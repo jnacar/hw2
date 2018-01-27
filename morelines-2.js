12a28f3eee7bf74d35594988ed3e33f88ec61768
jnacar <jefrey.nacar@berkeley.edu>
2018-01-27 14:42:34 -0800

    Update morelines-2.js

--- morelines-2.js
+++ morelines-2.js
@@ -33,3 +33,29 @@ function draw() {
 }
 
 \\ Part d
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+  var xfirst = random(width);
+  var yfirst = random(height);
+  var xsecond = random(width);
+  var ysecond = random(height);
+  var lineLength = Math.sqrt(Math.pow(xsecond - xfirst, 2) + Math.pow(ysecond - yfirst, 2));
+  if (lineLength < 120) {
+    stroke('red');
+  }
+  else if (lineLength < 240) {
+    stroke('yellow');
+  }
+  else if (lineLength < 360) {
+    stroke('blue');
+  }
+  else {
+    stroke('black');
+  }
+  
+  line(xfirst, yfirst, xsecond, ysecond);
+}
