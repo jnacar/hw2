15541706fb5ce97a888f50bdd8b66aedbb6497e7
jnacar <jefrey.nacar@berkeley.edu>
2018-01-31 00:53:13 -0800

    Update modern-dayMondrian-4.js

--- modern-dayMondrian-4.js
+++ modern-dayMondrian-4.js
@@ -1,3 +1,55 @@
+// Assignment 1
+
+createCanvas(340, 340);
+background(255, 255, 255);
+colorMode(RGB);
+strokeWeight(20);
+stroke(0,0,0);
+rect(0, 0, 340, 340);
+
+strokeWeight(10);
+fill(255, 255, 0);
+rect(6, 6, width/2, width/2)
+
+fill(0, 0, 0);
+rect(width/2+6, 6, 166, width/4)
+
+fill(0, 0, 255);
+rect(width/2+6, width/4+6, 160, width/4)
+
+fill(255, 0, 0);
+rect(width/2+6, width/2+6, width/2-12, width/2-12)
+
+line(0, 255, width/2+6, 255);
+
+
+// Assignment 2
+
+createCanvas(340, 340);
+background(255, 255, 255);
+colorMode(RGB);
+strokeWeight(20);
+stroke(0,0,0);
+rect(0, 0, 340, 340);
+
+strokeWeight(10);
+fill(random(255), random(255), random(255));
+rect(6, 6, width/2, width/2)
+
+fill(random(255), random(255), random(255));
+rect(width/2+6, 6, 166, width/4)
+
+fill(random(255), random(255), random(255));
+rect(width/2+6, width/4+6, 160, width/4)
+
+fill(random(255), random(255), random(255));
+rect(width/2+6, width/2+6, width/2-12, width/2-12)
+
+line(0, 255, width/2+6, 255);
+
+
+// Super-bonus Challenge
+
 createCanvas(340, 340);
 background(255, 255, 255);
 colorMode(RGB);
